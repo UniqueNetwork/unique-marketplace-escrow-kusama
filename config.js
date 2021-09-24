051f2536a0dab328f8dd0aa8687ab90383a3bacb
sotmorskiy <sotmorskiy@usetech.ru>
2021-09-24 14:12:18 +0700

    Readme.

--- config.js
+++ config.js
@@ -1,15 +1,20 @@
 const config = {
+  // quote network rpc.
   wsEndpoint : 'wss://kusama-rpc.polkadot.io',
 
+  // Seed phrase of the kusama escrow admin.
   adminSeed : process.env.ADMIN_SEED || '//Alice',
 
+  // pg sql connection.
   dbHost : process.env.DB_HOST || 'localhost',
   dbPort : process.env.DB_PORT || 5432,
   dbName : process.env.DB_NAME|| 'marketplace',
   dbUser : process.env.DB_USER || 'marketplace',
   dbPassword : process.env.DB_PASSWORD || '12345',
 
-  startFromBlock : process.env.START_FROM_BLOCK || 'current', // Either block number or 'current' to start from current block.
+  // From which block to start at the first run.
+  // Either block number or 'current' to start from current block.
+  startFromBlock : process.env.START_FROM_BLOCK || 'current',
 };
 
 module.exports = config;
